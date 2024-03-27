693953f8af3103aa4ec1439432b67fd6e1313a4b
Alex Hansen <hansen.alex@telia.com>
2024-03-27 16:24:04 +0100

    feat: loading state for jobs fetch with fake delay to visualize

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Loader } from "./Loader.tsx";
 import { JobPostData } from "./data.ts";
 import { JobPosts } from "./JobPosts.tsx";
 
 function App() {
   useEffect(() => {
     const getJobs = async (): Promise<void> => {
+      setIsLoading(true);
+
+      await new Promise((res) => setTimeout(res, 1000)); //Manual delay to test loading
+
       try {
         const response = await axios.get("src/assets/data/data.json");
         setJobsData(response.data.jobs);
@@ -13,16 +18,18 @@ function App() {
         // if(axios.isAxiosError(error)) {console.log(error as AxiosError)}
         console.log(error);
       }
+
+      setIsLoading(false);
     };
+
     getJobs();
   }, []);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [jobsData, setJobsData] = useState<JobPostData[]>([]);
 
   return (
-    <>
-      <JobPosts jobPostsData={jobsData} />
-    </>
+    <>{(isLoading && <Loader />) || <JobPosts jobPostsData={jobsData} />}</>
   );
 }
 
---
+++ src/Loader.tsx
@@ -0,0 +1,5 @@
+import style from "./Loader.module.css";
+
+export const Loader = () => {
+  return <h1 className={style.loader}>Loading...</h1>;
+};
